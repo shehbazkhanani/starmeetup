31f1ca28b9b88afbc70d05f9d0616191b9163dc8
shehbaz khanani <cuteboyusman49@gmail.com>
2023-03-19 13:33:24 +0500

    update

--- Screens/SplashScreen/index.tsx
+++ Screens/SplashScreen/index.tsx
@@ -1,39 +1,35 @@
-import { Image, StyleSheet, Text, View,Dimensions } from 'react-native'
-import React,{useEffect} from 'react'
-import LinearGradient from 'react-native-linear-gradient'
+import {Image, StyleSheet, Text, View, Dimensions} from 'react-native';
+import React, {useEffect} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
 
 const SplashScreen = ({navigation}: any) => {
-    const navigateToHomeScreen = () =>{
-        setTimeout(()=>{
-            navigation.navigate('Home')
-        },3000)
-    }
-    useEffect(()=>{
-        navigateToHomeScreen()
-    },[])
+  const navigateToHomeScreen = () => {
+    setTimeout(() => {
+      navigation.navigate('Home');
+    }, 3000);
+  };
+  useEffect(() => {
+    navigateToHomeScreen();
+  }, []);
   return (
-    <LinearGradient
-              colors={['#7F27F4', '#a266f4']}
-              style={[styles.gradient]}
-              start={{x: 0, y: 0}}
-              end={{x: 0, y: 1}}
-              useAngle
-              angle={0}>
-              <Image source={require('../../Images/Logo.png')} resizeMode='contain' style={styles.logo} />
-            </LinearGradient>
-  )
-}
+    <Image
+      source={require('../../Images/Logo.png')}
+      resizeMode="contain"
+      style={styles.logo}
+    />
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
 
 const styles = StyleSheet.create({
-    gradient: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      logo: {
-        height: Dimensions.get('window').height,
-        width: Dimensions.get('window').width,
-      },
-})
+  gradient: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+});
